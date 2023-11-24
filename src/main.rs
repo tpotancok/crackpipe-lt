b9d19d16c0c2ab00cd881fd89a08586aa9748376
Teodor Potancok <teodor.potancok@gmail.com>
2023-11-24 12:10:19 +0100

    Test downloading 2 torrents at the same time

--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,10 @@ use std::{fs, thread::sleep, time::Duration};
 
 use crackpipe_lt::ffi::{self, DownloadStatus};
 
-// public domain CD
+// Wired album (CC licensed)
 const MAGNET_LINK: &'static str = "magnet:?xt=urn:btih:a88fda5954e89178c372716a6a78b8180ed4dad3&dn=The+WIRED+CD+-+Rip.+Sample.+Mash.+Share&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fwired-cd.torrent";
+// Big Buck Bunny
+const MAGNET_LINK_2: &'static str = "magnet:?xt=urn:btih:dd8255ecdc7ca55fb0bbf81323d87062db1f6d1c&dn=Big+Buck+Bunny&tr=udp%3A%2F%2Fexplodie.org%3A6969&tr=udp%3A%2F%2Ftracker.coppersurfer.tk%3A6969&tr=udp%3A%2F%2Ftracker.empire-js.us%3A1337&tr=udp%3A%2F%2Ftracker.leechers-paradise.org%3A6969&tr=udp%3A%2F%2Ftracker.opentrackr.org%3A1337&tr=wss%3A%2F%2Ftracker.btorrent.xyz&tr=wss%3A%2F%2Ftracker.fastcast.nz&tr=wss%3A%2F%2Ftracker.openwebtorrent.com&ws=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2F&xs=https%3A%2F%2Fwebtorrent.io%2Ftorrents%2Fbig-buck-bunny.torrent" ;
 
 pub fn main() {
     let _ = fs::remove_dir_all("./output/");
@@ -11,6 +13,10 @@ pub fn main() {
     let torrent = ffi::add_torrent(
         session.pin_mut(),
         &*ffi::parse_magnet_link(MAGNET_LINK, "./output/"),
+    );
+    let torrent2 = ffi::add_torrent(
+        session.pin_mut(),
+        &*ffi::parse_magnet_link(MAGNET_LINK_2, "./output/2/"),
     );
     let mut open_torrents = 1u16;
     'outer: loop {
@@ -25,8 +31,12 @@ pub fn main() {
             }
         }
         println!(
-            "Torrent progress: {}%",
+            "Torrent 1 progress: {}%",
             ffi::status_get_progress(&ffi::get_torrent_status(&torrent)) * 100.
+        );
+        println!(
+            "Torrent 2 progress: {}%",
+            ffi::status_get_progress(&ffi::get_torrent_status(&torrent2)) * 100.
         );
         sleep(Duration::from_millis(500))
     }
